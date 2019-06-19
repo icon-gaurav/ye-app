faba81c9b243b95308d1fd968980905f69f9c7ee
icon-gaurav <icon.gaurav806@gmail.com>
2019-06-20 01:57:47 +0530

    added Navigation link to login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import ForgotPassword from "./component/common/login-module/ForgotPassword";
 import Registration from "./component/common/signup-module/Registration";
 import AuthorizedUser from "./AuthorizedUser";
 import ApiAction from "./actions/ApiAction";
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
+import StudentRegistration from "./component/common/signup-module/StudentRegistration";
+import BusinessRegistration from "./component/common/signup-module/BusinessRegistration";
 
 class App extends PureComponent {
     constructor(props) {
@@ -27,7 +31,7 @@ class App extends PureComponent {
                 if (response.data.success) {
                     localStorage.setItem("loggedIn", JSON.stringify(true));
                     localStorage.setItem("user", JSON.stringify(response.data.user));
-                    this.setState({logIn:true})
+                    this.setState({logIn: true})
                 } else {
                     localStorage.removeItem("loggedIn");
                     localStorage.removeItem("user");
@@ -45,11 +49,17 @@ class App extends PureComponent {
 
             logIn ? <AuthorizedUser/> :
                 <div className="d-flex justify-content-center bg-white vh-100">
-
-                    {forgotPass ? <ForgotPassword/> : signUp ? <Registration/> :
-                        <Login forgotPassword={() => this.setState({forgotPass: true})}
-                               signUp={() => this.setState({signUp: true})}
-                               logIn={() => this.setState({logIn: true})}/>}
+                    <BrowserRouter>
+                        <Route exact path={"/forgot-password"} component={() => <ForgotPassword/>}/>
+                        <Route exact path={"/register"} component={() => <Registration/>}/>
+                        <Route exact path={"/student-registration"} component={() => <StudentRegistration/>}/>
+                        <Route exact path={"/business-registration"} component={() => <BusinessRegistration/>}/>
+                        <Route exact path={"/"} component={() => <Login/>}/>
+                        {/*{forgotPass ? <ForgotPassword/> : signUp ? <Registration/> :*/}
+                        {/*    <Login forgotPassword={() => this.setState({forgotPass: true})}*/}
+                        {/*           signUp={() => this.setState({signUp: true})}*/}
+                        {/*           logIn={() => this.setState({logIn: true})}/>}*/}
+                    </BrowserRouter>
                 </div>
 
         );
